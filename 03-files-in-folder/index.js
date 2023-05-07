dee9c9a63f293caa30777a8df6e4d334e0e2395f
eugeniakhaleeva <khaleeva20990@gmail.com>
2023-05-07 20:05:12 +0200

    feat: add solution 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,29 @@
+const {readdir} = require('fs/promises');
+const {stat} = require('fs');
+const path = require('path');
+
+
+async function readFilesInFolder() {
+  const folderPath = path.join(__dirname, 'secret-folder');
+
+  try {
+    const files = await readdir(folderPath);
+    for (const file of files) {
+      stat(path.join(folderPath, file), (err, stats) => {
+
+        if(stats.isFile()){
+          console.log(`${file} является файлом:`);
+          console.log(`Наименование файла: ${file}`);
+          console.log(`Расширение файла: ${path.extname(file)}`);
+          console.log(stats);
+        } else {
+          console.log(`${file} не является файлом`);
+        }
+      });
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+readFilesInFolder().then(r => r);
